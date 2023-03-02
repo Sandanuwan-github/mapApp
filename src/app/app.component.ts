4f48ba1349c7f5eb8481d3ed14b123602dc4ed68
Sandanuwan <sandanuwan.dh@gmail.com>
2023-03-02 22:28:15 +0530

    added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,39 @@ export class AppComponent implements OnInit {
   title = 'mapApp';
 
   map!: mapboxgl.Map;
-  //style = 'mapbox://styles/mapbox/streets-v11';
+  // if remove this. map will not show in browser (free difined core style)
   style = 'mapbox://styles/mapbox/streets-v12';
   lat = 10.746551;
   lng = 59.910742;
-  mapStyle = 'naturalEarth';
 
   ngOnInit() {
+    // get access token
     mapboxgl!.accessToken = environment.mapbox.accessToken;
     this.map = new mapboxgl.Map({
+      // where need to show map. inside the div we can pass this map as id
       container: 'map',
       style: this.style,
-      //zoom: 13,
       zoom: 8,
+      // map start point
       center: [this.lat, this.lng],
     });
 
-    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      'Hi sanda'
-    )
+    // add pop up
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText('Hi sanda');
 
-    new mapboxgl.Marker().setLngLat([this.lat, this.lng]).addTo(this.map).setPopup(popup);
+    // add pin
+    new mapboxgl.Marker({
+      color: '#eb4034',
+      scale: 1.1
+    })
+      .setLngLat([this.lat, this.lng])
+      .addTo(this.map)
+      .setPopup(popup);
 
+    // set map looks
     this.map.setProjection('globe');
-    // Add map controls
+
+    // Add map controls - zoom option
     this.map.addControl(new mapboxgl.NavigationControl());
   }
 }
